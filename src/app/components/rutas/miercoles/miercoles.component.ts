ead17c825a3ee971cfadcd2f77aebddc02e59c1c
Cristian <ccristiandarioromero@gmail.com>
2020-12-08 18:49:42 -0300

    Atendido

--- src/app/components/rutas/miercoles/miercoles.component.ts
+++ src/app/components/rutas/miercoles/miercoles.component.ts
@@ -17,97 +17,7 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class MiercolesComponent implements OnInit {
   productList: Product[];
-  usuario: any;
-  usuarios: any[] = [
-  /*  { id: 1, nombre:'Andres',img:'../../../assets/img/m1.png',img2:'../../../assets/img/p1.jpg' },
-    { id: 2, nombre:'Tomy', img:'../../../assets/img/m2.png', img2:'../../../assets/img/p2.jpg'},
-    { id: 3, nombre:'Cristiano',img:'../../../assets/img/m1.png',img2:'../../../assets/img/p3.jpg' },
-    { id: 4, nombre:'Lio',img:'../../../assets/img/m1.png',img2:'../../../assets/img/p4.jpg' }*/
-    {
-      direccion: "General hornos 1975",
-      id: 23,
-      localidad: "Caseros",
-      name: "Araldo",
-      observacion: "1",
-      pago: "Mes",
-      price: 60,
-      ruta: "1",
-      telefono: 47522250,
-      tipo: "Miércoles"
-    },
-    {
-      direccion : "Sabattini 5681",
-      id : 24,
-    localidad : "MartiCoronado",
-      name : "Romina ",
-      observacion: "1",
-      pago: "Mes",
-    price: 250,
-      ruta: "1",
-      telefono: 1150011074,
-      tipo: "Miércoles"
-    },
-    {
-      direccion: "Pringles 3577",
-    id: 27,
-      localidad: "Caseros",
-      name: "Belén",
-      observacion: "1",
-      pago: "Mes",
-      price: 170,
-      ruta: "2",
-      telefono: 1151414818,
-      tipo: "Miércoles"
-    },
-    {
-      direccion: "Año 1852 460",
-      id: 28,
-      localidad: "Palomar",
-      name: "Alicia",
-      observacion: "1",
-      pago: "Mes",
-      price: 250,
-      ruta: "1",
-      telefono: 1168321886,
-      tipo : "Miércoles"
-    },
-    {
-      direccion: "Guido Spano 1813",
-      id: 52,
-      localidad: "Martin Coronado",
-      name: "Mirta Ayala",
-      observacion: "2",
-      pago: "1",
-      price: 280,
-      ruta: "B 12Lts",
-      telefono: 1143998042,
-      tipo: "Miércoles"
-    },
-   {
-      direccion: "Sargento Palma 1260",
-      id: 53,
-      localidad: "Martin Coronado",
-      name: "Lorena",
-      observacion: "2",
-      pago: "1",
-      price: 220,
-      ruta: "Bs 12Lts",
-      telefono: 1131299245,
-      tipo: "Miércoles"
-    },
-    {
-      direccion: "Rio Negro 1880",
-      id: 55,
-      localidad: "Palomar",
-      name: "Sonia",
-      observacion: "1",
-      pago: "1",
-      price: 1120,
-      ruta: "B 12Lts",
-      telefono: 1125870372,
-      tipo: "Miércoles"
-    }
-  ] 
+   
   
     constructor( private productService: ProductService,
       private modalService: NgbModal,
@@ -126,8 +36,8 @@ export class MiercolesComponent implements OnInit {
       }); 
     }
   
-    ver(usuario: any, modal){
-      this.usuario = usuario;
+    ver( modal){
+      
       this.modalService.open(modal);
     }
   
--- src/app/components/rutas/viernes/viernes.component.ts
+++ src/app/components/rutas/viernes/viernes.component.ts
@@ -1,195 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Product } from '../../../models/product';
+// service
+import { ProductService } from '../../../services/product.service';
 
-import * as $ from "jquery";
+// toastr
+import { ToastrService } from 'ngx-toastr';  
 @Component({
   selector: 'app-viernes',
   templateUrl: './viernes.component.html',
   styleUrls: ['./viernes.component.css']
 })
 export class ViernesComponent implements OnInit {
-   
+  productList: Product[]; 
   usuario: any;
-  usuarios: any[] = [
-  /*  { id: 1, nombre:'Andres',img:'../../../assets/img/m1.png',img2:'../../../assets/img/p1.jpg' },
-    { id: 2, nombre:'Tomy', img:'../../../assets/img/m2.png', img2:'../../../assets/img/p2.jpg'},
-    { id: 3, nombre:'Cristiano',img:'../../../assets/img/m1.png',img2:'../../../assets/img/p3.jpg' },
-    { id: 4, nombre:'Lio',img:'../../../assets/img/m1.png',img2:'../../../assets/img/p4.jpg' }*/
-    {
-      direccion: "Vuelta de obligado 4885",
-      id: 21,
-      localidad : "VillaBosch",
-      name : "Ricardo",
-      observacion: "1",
-      pago: "Mes",
-      price: 250,
-      ruta: "1",
-      telefono: 5493751613309,
-      tipo: "Viernes"
-    },
-    {
-      direccion: "Rivadavia 3745 6to \"D\"",
-      id: 29,
-      localidad: "SanMartin",
-      name: "Eduardo",
-      observacion: "1",
-      pago: "Mes",
-      price: 240,
-      ruta: "2",
-      telefono: 1164545723,
-      tipo: "Viernes"
-    },
-    {
-      direccion: "Av. De los Constituyentes",
-      id: 38,
-      localidad: "SanMartin",
-      name: "Alejandra",
-      observacion: "1",
-      pago: "Mes",
-      price: 220,
-      ruta: "1",
-      telefono: 1138225293,
-      tipo: "Viernes"
-    },
-    {
-      direccion: "Pueyrredón 2800",
-      id: 45,
-      localidad: "SanMartin",
-      name: "Yanina",
-      observacion: "1",
-      pago: "Mes",
-      price: 190,
-      ruta: "1",
-      telefono: 1123890229,
-      tipo: "Viernes"
-    },
-    {
-      direccion: "Canesse 3567",
-      id: 46,
-      localidad: "San martin",
-      name: "Martin alfeo",
-      observacion: "2",
-      pago: "1",
-      price: 275,
-      ruta: "B 12Lts",
-      telefono: 1158340588,
-      tipo: "Viernes"
-    },
-    {
-      direccion: "Alvear 3953",
-      id: 50,
-      localidad: "Billinghurst",
-      name: "Juan",
-      observacion: "2",
-      pago: "1",
-      price: 280,
-      ruta: "B 12Lts",
-      telefono: 1167995252,
-      tipo: "Viernes"
-    },
-   {
-      direccion: "Rivadavia 3745 6to \"C\"",
-      id: 51,
-      localidad: "San martin",
-      name: "Dalia",
-      observacion: "2",
-      pago: "1",
-      price: 240,
-      ruta: "B 12Lts",
-      telefono: 1121806560,
-      tipo: "Viernes"
-    },
-   {
-      direccion: "Pellegrini 1792 \"Pb\"",
-      id: 52,
-      localidad: "San martin",
-      name: "Carlos",
-      observacion: "2",
-      pago: "1",
-      price: 250,
-      ruta: "B 12Lts",
-      telefono: 1133012860,
-      tipo: "Viernes"
-    },
-    {
-      direccion: "San Lorenzo 2272 3ro \"B\"",
-      id: 54,
-      localidad: "San martin",
-      name: "Cristian",
-      observacion: "3",
-      pago: "1",
-      price: 210,
-      ruta: "B 12Lts",
-      telefono: 1161823876,
-      tipo: "Viernes"
-    },
-    {
-      direccion: "Juárez 3758 t B",
-      id: 57,
-      localidad: "San martin",
-      name: "Nancy",
-      observacion: "3",
-      pago: "1",
-      price: 290,
-      ruta: "B 12Lts",
-      telefono: 5493437449915,
-      tipo: "Viernes"
-    },
-     {
-      direccion: "San Luis 3564",
-      id: 58,
-      localidad: "San Andrès",
-      name: "Liliana",
-      observacion: "2",
-      pago: "1",
-      price: 250,
-      ruta: "B 12Lts",
-      telefono: 1138488758,
-      tipo: "Viernes"
-    },
-    {
-      direccion: "Gral. Güemes 3820 ",
-      id: 59,
-      localidad: "San martin",
-      name: "Lorena",
-      observacion: "2",
-      pago: "1",
-      price: 1120,
-      ruta: "B 12Lts",
-      telefono: 1139188192,
-      tipo: "Viernes"
-    },
-    {
-      direccion: "Pueyrredón 2610 t izq",
-      id: 60,
-      localidad: "San martin",
-      name: "Monica",
-      observacion: "2",
-      pago: "1",
-      price: 250,
-      ruta: "B 12Lts",
-      telefono: 1133591935,
-      tipo: "Viernes"
-    },
-    {
-      direccion: "José María Campos 1664",
-      id: 62,
-      localidad: "San martin",
-      name: "Marcelo",
-      observacion: "2",
-      pago: "1",
-      price : 250,
-      ruta: "B 12Lts",
-      telefono: 1164779576,
-      tipo: "Viernes"
-    }
-  ] 
-  
-    constructor(private modalService: NgbModal) { }
+   
+    constructor( private productService: ProductService,
+      private modalService: NgbModal,
+      private toastr: ToastrService) { }
     closeResult = '';
   
     ngOnInit() {
-      
+              return this.productService.getProducts()
+              .snapshotChanges().subscribe(item => {
+                this.productList = [];
+                item.forEach(element => {
+                  let x = element.payload.toJSON();
+                  x["$key"] = element.key;
+                  this.productList.push(x as Product);
+                });
+              }); 
            }
   
     ver(usuario: any, modal){
@@ -206,5 +46,10 @@ export class ViernesComponent implements OnInit {
         return `with: ${reason}`;
       }
     }
+  
+    selectedIdx = 0;
 
+    selectItem(index):void {
+        this.selectedIdx = index;
+    } 
 }
